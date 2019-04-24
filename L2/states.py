6192de2d926112d4db2a914c04cf8d53d85a9929
Martin Vinay <mlvinay@uc.cl>
2019-04-24 15:40:00 -0400

    util: timer, states: done

--- L2/states.py
+++ L2/states.py
@@ -1,21 +1,35 @@
-from util import pi_fix
+from util import pi_fix, Timer
 import numpy as np
+from classes import State
+
+
 import rospy
+from nav_msgs.msg import Odometry
+
 
 class States():
 
     def __init__(self):
+        self.timer = Timer()
         ## 0 = xpos, 1 =  ypos, 2 = zpos, 3 = angular pos
         self.zero = [0 for i in range(4)]
         ## 0 = xpos, 1 = ypos, 2 = zpos, 3 = ang pos.
         self.pos = [0 for i in range(4)]
+        self.speed = [0 for i in range(4)]
         #First oddometry read to get references
         self.ref = True
 
+
+
         ## Subscribe to odometry
         rospy.Subscriber( 'odom', Odometry, self.Odom_read)
+        self.publisher = rospy.Publisher('states',State)
 
-
+        rospy.on_shutdown( self.shutdown )
+        self.r = rospy.Rate(30);
+        while not rospy.is_shutdown:
+            new_item = State(self.pos[0],self.pos[1],self.pos[3],self.speed[0],self.speed[1],self.speed[3])
+            self.publisher.publish()
 
 
     ##Odometry message read
@@ -34,6 +48,7 @@ class States():
 
             self.zero[3] = 2*np.arccos( angaux ) if (odom_data.pose.pose.orientation.w < 0) else -2*np.arccos( angaux )
             self.ref = False
+            self.timer.reset()
 
 
 
@@ -48,6 +63,20 @@ class States():
         angaux2 = -2*np.arccos( angaux ) if (odom_data.pose.pose.orientation.z < 0) else 2*np.arccos( angaux )
         new_ang = pi_fix(angaux2-self.zero[3])
 
+
+        ## Calculate speeds
+        if self.timer.time()>10:
+            x_speed = 1000*(new_x - self.pos[0])/self.timer.time()
+            y_speed = 1000*(new_y - self.pos[1])/self.timer.time()
+            z_speed = 1000*(new_z - self.pos[2])/self.timer.time()
+            ang_speed = 1000*(new_ang - self.pos[3])/self.timer.time()
+            self.timer.reset()
+
+            ##Actualize new speeds
+            self.speed[0] = x_speed
+            self.speed[1] = y_speed
+            self.speed[2] = z_speed
+            self.speed[3] = ang_speed
 
         ##Actualize new values
         self.pos[0] = new_x
--- L2/util.py
+++ L2/util.py
@@ -1,3 +1,6 @@
+import numpy as np
+import rospy
+
 def pi_fix(angle_in):
     if angle_in<-np.pi:
         sub = angle_in+2*np.pi
@@ -7,20 +10,31 @@ def pi_fix(angle_in):
         sub = angle_in
     return sub
 
-class Angle():
+
+class Timer():
     def __init__(self):
-        self._value = 0
+        self.ref = rospy.Time.now().to_msec()
 
+    def reset(self):
+        self.ref = rospy.Time.now().to_msec()
 
+    def.time(self):
+        return rospy.Time.now().to_msec() - ref
 
-
-    def self.value():
-        doc = "The  property."
-        def fget(self):
-            return self._value
-        def fset(self, value):
-            self._value = value
-        def fdel(self):
-            del self
-        return locals()
-     = property(**())
+# class Angle():
+#     def __init__(self):
+#         self._value = 0
+#
+#
+#
+#
+#     def self.value():
+#         doc = "The  property."
+#         def fget(self):
+#             return self._value
+#         def fset(self, value):
+#             self._value = value
+#         def fdel(self):
+#             del self
+#         return locals()
+#      = property(**())
