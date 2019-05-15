df2bb97b304e5668bb3ba2ae20ab72dca5b10d23
Noemi Crosby <nicrosby@uc.cl>
2019-05-15 11:46:04 -0400

    Changes Pasillo

--- script/Main.py
+++ script/Main.py
@@ -2,10 +2,7 @@
 import rospy
 import math
 import roslib
-#from Movement import Movement
-#from ComputerVision import Vision
 from Pasillo import Pasillo
-#from turtlebot_audio import TurtlebotAudio
 from sensor_msgs.msg import Image
 from std_msgs.msg import Empty
 import numpy as np
@@ -17,8 +14,6 @@ from cv_bridge import CvBridge, CvBridgeError
 class Turtlebot(object):
 
 	def __init__( self ):
-		#self.movement = Movement()
-		# self.vision = Vision(self.movement)
 		self.pasillo = Pasillo()
 		rospy.sleep(1)
 
