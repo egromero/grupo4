b8388168faa73878702565dd13d0aba529673e55
Martin Vinay <mlvinay@uc.cl>
2019-05-17 11:27:12 -0400

    Fixed main and obstaculos.py

--- scripts/Main.py
+++ scripts/Main.py
@@ -8,51 +8,73 @@ from std_msgs.msg import String, Bool
 
 route = [[1,0,None],[0,0,None],[0,0,0]]
 
+
 class Turtlebot(object):
 	def __init__( self ):
-		## bad joke.
+		## item are main targets, subroutes are if obstacles are detected, allows nesting (obsacle in first obstacle route)
+		self.item = None
+		self.sub_route = []
 		self.target_publisher = rospy.Publisher('new_target',String,queue_size=10)
 		rospy.sleep( 0.2 )
-		self.obstacle = False
+		# self.obstacle = False
+
 
 		rospy.Subscriber('target_reached',Bool,self.target_reached_callback)
 		rospy.Subscriber('route_obstacle', String, self.new_route_recieved)
 		rospy.sleep( 0.2 )
 
-		self.flag = True
+		self.target_reached_flag = True
 		self.r = rospy.Rate(5)
 		for item in route:
+			## Allows for better progaming, instead of nesting with if, a smarter choice would choose between publishing main targets or secondary targets
+			self.item = item
+
 			encoded = json.dumps(item)
 			self.target_publisher.publish(encoded)
-			print('Sent {}'.format(encoded))
-			self.flag = False
 
-			while not self.flag and not rospy.is_shutdown():
+			print('Sent {}'.format(encoded))
+
+			self.target_reached_flag = False
+			## Only move through main targets if all subroute targets have been cleared
+			self.item_ready = self.target_reached_flag and (len(self.sub_route)==0)
+
+			while not self.item_ready and not rospy.is_shutdown():
 				#print('Actual flag' ,self.flag)
-				if self.obstacle:
-					print('Obstaculo')
-					while len(self.new_route) >= 1:
-						instruction = self.new_route.pop(0)
-						encoded = json.dumps(instruction)
-						self.target_publisher.publish(encoded)
-						print('Sent {}'.format(encoded))
-						self.flag = False
-						while not self.flag and not rospy.is_shutdown():
-							print('Esquivando')
-							print("actual flag: ",self.flag)
-							self.r.sleep()
-					self.obstacle = False
+
+				## if target reached and there still are sub_route targets, get a new one
+				if self.target_reached and (len(self.sub_route)!=0):
+					instruction = self.sub_route.pop(0)
+					encoded = json.dumps(instruction)
+					self.target_publisher.publish(encoded)
+					print('Sent {}'.format(encoded))
+
+					self.target_reached = False
 				self.r.sleep()
+				# if self.obstacle:
+				# 	print('Obstaculo')
+				# 	while len(self.new_route) >= 1:
+				# 		instruction = self.new_route.pop(0)
+				# 		encoded = json.dumps(instruction)
+				# 		self.target_publisher.publish(encoded)
+				# 		print('Sent {}'.format(encoded))
+				# 		self.flag = False
+				# 		while not self.flag and not rospy.is_shutdown():
+				# 			print('Esquivando')
+				# 			print("actual flag: ",self.flag)
+				# 			self.r.sleep()
+				# 	self.obstacle = False
+				# self.r.sleep()
 
 	def target_reached_callback(self,data):
-		self.flag = data.data
+		self.target_reached_flag = data.data
 		#print('incoming flag :',data.data)
+	# def new_route_recieved(self, data):
+	# 	if not self.obstacle:
+	# 		self.new_route = json.loads(data.data)
+	# 		self.obstacle = True
 	def new_route_recieved(self, data):
-		if not self.obstacle:
-			self.new_route = json.loads(data.data)
-			self.obstacle = True
+		self.sub_route =  json.loads(data.data).append(self.item)
 
-		
 if __name__ == '__main__':
 	rospy.init_node( "turtlebot_g4" )
 	handler = Turtlebot()
--- scripts/obstaculos.py
+++ scripts/obstaculos.py
@@ -12,7 +12,7 @@ CENTER = [240,320]
 VISION_WINDOW = [100,90]
 
 class DetectorObstaculos(object):
-	
+
 	def __init__(self):
 		print("iniciando obstaculos")
 		self.bridge = CvBridge()
@@ -21,20 +21,32 @@ class DetectorObstaculos(object):
 		self.state_odom = rospy.Subscriber('our_state',String, self.data_received)
 
 	def process(self, data):
+		# New image callback
+		# Looks if obstacle is on defined window.
 		self.depth_image = np.nan_to_num(np.asarray( self.bridge.imgmsg_to_cv2( data, "32FC1" )))
 		sub_image = self.depth_image[CENTER[0]-VISION_WINDOW[0] : CENTER[0]+VISION_WINDOW[0], CENTER[1]-VISION_WINDOW[1] :CENTER[1]+VISION_WINDOW[1]]
 		self.obstacle = ( np.greater( sub_image, 0.2 ) * np.less( sub_image, 0.6 ) ).any()
 		if self.obstacle:
 			self.calculate_route()
-	
+
 	def data_received(self, data):
+		## Odometry callback for rerouting planning
 		inc_dict = json.loads(data.data)
         	self.x = inc_dict['x']
         	self.y = inc_dict['y']
        		self.ang = inc_dict['ang_pos']
 
 	def calculate_route(self):
-		new_route = [[self.x, self.y+0.5, self.ang],[self.x+0.5, self.y+0.5, self.ang],[self.x+0.5, self.y, self.ang]]
+		## Create rot rot_matrix
+		#	Calculate relative 0.5 x,y axis Movement
+		#	applicate to old vector and publish in corresponding topic after json encoding
+		rot_matrix = np.array([[np.cos(self.ang),np.sin(self.ang)],[-np.sin(self.ang),np.cos(self.ang)]])
+		extra_x = np.matmul(rot_matrix,np.array([[0.5],[0]]))
+		extra_y = np.matmul(rot_matrix,np.array([[0],[0.5]]))
+		original_vec = np.array([[self.x],[self.y]])
+		new_vectors = [original_vec + extra_y, original_vec+ extra_y + extra_x]
+		new_route = [ [new_vectors[0][0],new_vectors[0][1],None] ,[new_vectors[1][0],new_vectors[1][1] ],None]
+		print(new_route)
 		encoded = json.dumps(new_route)
 		self.pub_obstacle.publish(encoded)
 
@@ -43,5 +55,3 @@ if __name__ =='__main__':
     rospy.init_node("obstacles")
     handler = DetectorObstaculos()
     rospy.spin()
-
-
