9539c34dc3b08407f460e82f6618a69d43dc65a2
Martin Vinay <mlvinay@uc.cl>
2019-06-17 17:15:25 -0400

    data to image completed

--- scripts/data_to_image.py
+++ scripts/data_to_image.py
@@ -1,9 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+import cv2
 
-from scipy.ndimage.interpolation import geometric_transform
+
+from scipy.ndimage.interpolation import rotate
 path = 'Measures/'
-name = 'measures_2'
+name = 'measures_3'
 sufix = '.txt'
 
 
@@ -13,6 +16,8 @@ valid = [angles[0]+window,angles[1]-window]
 max = 1 # max distance, set by sensor
 resolution = 0.005 # resolution of generated image, bigger = more time expensive
 magic_number = int(max/resolution) + 1
+offset = 90
+
 
 def remap(rmatrix,coords):
     global valid
@@ -25,7 +30,7 @@ def remap(rmatrix,coords):
         try:
             theta = (np.arctan2(y,x)+np.pi/2)*360/(2*np.pi)
             # print(theta)
-            if valid[0]<theta<valid[1]:
+            if valid[0]<theta<valid[1]:  #bIlineal interpolation
                 radius_index = radius/resolution
                 r1 = int(radius_index); r2 = r1+1
                 t1 = int(theta); t2 = t1+1
@@ -42,7 +47,7 @@ def remap(rmatrix,coords):
                 #     print(output[0])
                 output = output[0]
                 if output>0.5:
-                    output = output*1.3
+                    output = output*1.3 ## augment with saturation
                     if output>1:
                         output = 1
                 return output
@@ -63,8 +68,11 @@ with open (path+name+sufix) as file:
 sample = data[2]
 
 
+## Generate radial matrix
+tic = time.time()
 radial_vector = np.arange(0,max+resolution,resolution)
 radial_matrix = np.zeros([magic_number,angles[1]-angles[0]+1])
+
 
 for i in range(angles[1]-angles[0]+1):
     binary_vector = radial_vector>sample[i]
@@ -72,17 +80,43 @@ for i in range(angles[1]-angles[0]+1):
     rolled_vector[0]=False
 
     radial_matrix[:,i] = np.transpose(binary_vector*1 - rolled_vector*0.5)
-# x_array,y_array = np.mgrid[0:max+resolution:resolution,0:max+resolution:resolution]
+toc = time.time()-tic
+print('Time for radial matrix generation: ',toc)
+
+## Generate cartesian matrix
+tic = time.time()
 cart_matrix = np.mgrid[-max:max+resolution:resolution,0:max+resolution:resolution].reshape(2,-1).T
-#cart_matrix.shape = (401,401)
-
-# cart_matrix = np.indices([20,20]
-# xx , yy = np.meshgrid(radial_vector,radial_vector)
-# cart_matrix = zeros()
-
-
 end_vector = [remap(radial_matrix,item) for item in cart_matrix]
 end_matrix = np.reshape(np.array(end_vector),[magic_number*2-1,magic_number])
+toc = time.time()-tic
+print('Time for cartesian matrix generation: ',toc)
 
-plt.imshow(end_matrix)
+
+## Reduce cartsian matrix
+original_center =  np.array([int(max/resolution) + 1,0])
+
+tic = time.time()
+boolean_matrix1 = end_matrix!=0.5
+boolean_vector1 = [np.sum(item) for item in boolean_matrix1]
+boolean_index1 = np.nonzero(boolean_vector1);
+zero_min1 = boolean_index1[0][0]; zero_max1 = boolean_index1[0][-1]
+
+boolean_matrix2 = end_matrix.transpose()!=0.5
+boolean_vector2 = [np.sum(item) for item in boolean_matrix2]
+boolean_index2 = np.nonzero(boolean_vector2);
+zero_min2 = boolean_index2[0][0]; zero_max2 = boolean_index2[0][-1]
+
+new_center = original_center - np.array([zero_min1,0])
+new_matrix = end_matrix[zero_min1:zero_max1,zero_min2:zero_max2]
+toc = time.time()-tic
+print('Time for reducing cartesian matrix: ',toc)
+
+
+# rotated_matrix = rotate(new_matrix,30)
+
+plt.figure()
+plt.imshow(new_matrix)
+
+# plt.figure()
+# plt.imshow(dst)
 plt.show()
